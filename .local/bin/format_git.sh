5cf0c48388418a8d9be56963475839d17b866af1
Linus Boehm <linusboehm@gmail.com>
2024-04-17 15:15:34 +0000

    update format_git

--- .local/bin/format_git.sh
+++ .local/bin/format_git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # skip first two lines (commit message and empty line)
-read LINE
-echo $LINE
-read LINE
-echo $LINE
+read -r LINE
+echo "$LINE"
+read -r LINE
+echo "$LINE"
 
 INPUT=$(cat)
 
@@ -19,4 +19,17 @@ if [[ $COMMENT_LINE -ne 1 ]]; then
 fi
 
 # just echo the rest unchanged
-echo "$INPUT" | tail -n +$COMMENT_LINE
+echo "$INPUT" | tail -n +"$COMMENT_LINE"
+
+cat << EOF
+#
+# ---
+# feat: (new feature for the user, not a new feature for build script)
+# fix: (bug fix for the user, not a fix to a build script)
+# docs: (changes to the documentation)
+# style: (formatting, missing semi colons, etc; no production code change)
+# refactor: (refactoring production code, eg. renaming a variable)
+# test: (adding missing tests, refactoring tests; no production code change)
+# chore: (updating grunt tasks etc; no production code change)"""
+# ---
+EOF
